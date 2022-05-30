b42bebe386bee11cb2cc5455cce7f37870ae289e
Bruno Marques <blmarques.dev@gmail.com>
2022-05-30 00:23:47 -0300

    feat(type-for-api): adding type for response of the api

---
+++ src/@types/types.ts
@@ -0,0 +1,47 @@
+
+export type ytVideo = {
+  kind: string;
+  etag: string;
+  id: {
+    kind: string;
+    videoId: string;
+  };
+  snippet: {
+    publishedAt: string;
+    channelId: string;
+    title: string;
+    description: string;
+    thumbnails: {
+      default: {
+        url: string;
+        width: number;
+        height: number;
+      };
+      medium: {
+        url: string;
+        width: number;
+        height: number;
+      };
+      high: {
+        url: string;
+        width: number;
+        height: number;
+      };
+    };
+    channelTitle: string;
+    liveBroadcastContent: string;
+    publishTime: string;
+  };
+};
+
+
+export interface DataGoogleApi {
+  kind: string;
+  etag: string;
+  regionCode: string;
+  pageInfo: {
+    totalResults: number;
+    resultsPerPage: number;
+  };
+  items: ytVideo[];
+}
