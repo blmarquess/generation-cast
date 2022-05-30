762a252abd85d56c7ac8acfcb664ef1be643f61b
Bruno Marques <blmarques.dev@gmail.com>
2022-05-30 00:16:17 -0300

    feat(styled): config to correct functionality of styled-components

---
+++ src/pages/_app.tsx
@@ -0,0 +1,18 @@
+import type { AppProps } from 'next/app'
+import { ThemeProvider } from 'styled-components'
+
+import { GlobalStyle } from '../styles/global'
+import theme from '../styles/theme'
+
+
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <ThemeProvider theme={theme}>
+      <Component {...pageProps} />
+      <GlobalStyle />
+    </ThemeProvider>
+  )
+}
+
+export default MyApp
