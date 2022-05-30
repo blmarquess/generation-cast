926e2cb8008e5b4dec98cbd39dcad941666a3ee5
Bruno Marques <blmarques.dev@gmail.com>
2022-05-30 00:14:19 -0300

    chore(configs=project): add dependencies and configs to project

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  compiler: {
+    // ssr and displayName are configured by default
+    styledComponents: true,
+  },
+  images: {
+    domains: ["i.ytimg.com", "yt3.ggpht.com"],
+  }
 }
 
 module.exports = nextConfig
